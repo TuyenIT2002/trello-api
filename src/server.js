976b9b023f6226db593a18dc10b0fa11142d87e1
TuyenIT2002 <tuyenlinh15081405@gmail.com>
2025-02-11 15:09:46 +0700

    connect-mongodb-env

--- src/server.js
+++ src/server.js
@@ -1,15 +1,36 @@
-// const express = require('express')
 import express from 'express'
+import {CONNECT_DB,CLOSE_DB, GET_DB} from './config/mongodb'
+import exitHook from 'async-exit-hook'
+//import để sử dụng biến môi trường
+import 'dotenv/config'
+import {env} from '~/config/environment'
 
-const app = express()
-const hostname = 'localhost'
-const port = 3001
+const START_SERVER = ()=>{
+  const app = express()
 
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', function (req, res) {
-    res.send('Xin chao Tuyen')
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello ${env.AUTHOR}, I am running at ${ env.APP_HOST }:${ env.APP_PORT }/`)
+  })
 
-app.listen(port, hostname, ()=>{
-    console.log(`running seever at ${hostname}: ${port}`);
-})
+  exitHook(()=>{
+    CLOSE_DB()
+  })
+
+}
+
+(async ()=>{
+  try {
+    console.log('dang ket noi toi server');
+  await CONNECT_DB()
+    console.log('ket noi thanh cong')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
